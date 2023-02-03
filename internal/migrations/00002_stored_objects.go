d405b24ab8a852a20bd8bfc79a3b4745b5be5d4c
SpaceHead1C <damh17@gmail.com>
2023-02-03 20:09:15 +0300

    style: query text style

--- internal/migrations/00002_stored_objects.go
+++ internal/migrations/00002_stored_objects.go
@@ -13,14 +13,14 @@ func init() {
 func up00002(tx *sql.Tx) error {
 	query := `-- Stored objects
 CREATE TABLE IF NOT EXISTS records (
-	id uuid PRIMARY KEY
-	, reference_type_id uuid
-	, "name" varchar(128) NOT NULL DEFAULT ''
-	, description varchar(1024) NOT NULL DEFAULT ''
-	, deletion_mark bool NOT NULL DEFAULT false
-	, CONSTRAINT fk_reference_type
-			FOREIGN KEY(reference_type_id)
-				REFERENCES reference_types(id)
+	id uuid PRIMARY KEY,
+	reference_type_id uuid,
+	"name" varchar(128) NOT NULL DEFAULT '',
+	description varchar(1024) NOT NULL DEFAULT '',
+	deletion_mark bool NOT NULL DEFAULT false,
+	CONSTRAINT fk_reference_type
+		FOREIGN KEY(reference_type_id)
+			REFERENCES reference_types(id)
 );`
 	return execQuery(query, tx)
 }
