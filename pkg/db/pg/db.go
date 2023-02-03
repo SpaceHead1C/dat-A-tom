9edf8599096163037f48da98fd673e726f4eea1e
SpaceHead1C <damh17@gmail.com>
2023-02-03 12:49:46 +0300

    feat: add SSLMode for connection string

--- pkg/db/pg/db.go
+++ pkg/db/pg/db.go
@@ -7,12 +7,34 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+type SSLMode uint
+
+const (
+	SSLDisable SSLMode = iota
+	SSLRequire
+	SSLVerifyCA
+	SSLVerifyFull
+)
+
+func (m SSLMode) String() string {
+	switch m {
+	case SSLRequire:
+		return "require"
+	case SSLVerifyCA:
+		return "verify-ca"
+	case SSLVerifyFull:
+		return "verify-full"
+	}
+	return "disable"
+}
+
 type Config struct {
 	Address      string
 	Port         uint
 	User         string
 	Password     string
 	DatabaseName string
+	SSLMode      SSLMode
 }
 
 func NewDB(ctx context.Context, c Config) (*pgx.Conn, error) {
