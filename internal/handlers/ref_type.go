89a3ff54802762a14d295c0aadf948eb48ae9379
SpaceHead1C <damh17@gmail.com>
2023-02-09 23:50:26 +0300

    feat: update ref type handler

--- internal/handlers/ref_type.go
+++ internal/handlers/ref_type.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"context"
 	"datatom/internal/api"
+	"datatom/internal/domain"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -14,5 +17,30 @@ func AddRefType(ctx context.Context, man *api.RefTypeManager, req AddRefTypeRequ
 		return out, err
 	}
 	out.Payload = id.String()
+	return out, nil
+}
+
+func UpdateRefType(ctx context.Context, man *api.RefTypeManager, req UpdRefTypeRequestSchema) (Result, error) {
+	out := Result{Status: http.StatusNoContent}
+	if req.Name == nil {
+		out.Status = http.StatusBadRequest
+		return out, fmt.Errorf("name %w", domain.ErrExpected)
+	}
+	if req.Description == nil {
+		out.Status = http.StatusBadRequest
+		return out, fmt.Errorf("description %w", domain.ErrExpected)
+	}
+	r, err := req.UpdRefTypeRequest()
+	if err != nil {
+		out.Status = http.StatusBadRequest
+		return out, err
+	}
+	if _, err := man.Update(ctx, r); err != nil {
+		out.Status = http.StatusInternalServerError
+		if errors.Is(err, domain.ErrNotFound) {
+			out.Status = http.StatusNotFound
+		}
+		return out, err
+	}
 	return out, nil
 }
--- internal/handlers/ref_type_schema.go
+++ internal/handlers/ref_type_schema.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "datatom/internal/domain"
+import (
+	"datatom/internal/domain"
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 type AddRefTypeRequestSchema struct {
 	Name        string `json:"name"`
@@ -12,4 +17,23 @@ func (s AddRefTypeRequestSchema) AddRefTypeRequest() domain.AddRefTypeRequest {
 		Name:        s.Name,
 		Description: s.Description,
 	}
+}
+
+type UpdRefTypeRequestSchema struct {
+	ID          string  `json:"id"`
+	Name        *string `json:"name,omitempty"`
+	Description *string `json:"description,omitempty"`
+}
+
+func (s *UpdRefTypeRequestSchema) UpdRefTypeRequest() (domain.UpdRefTypeRequest, error) {
+	out := domain.UpdRefTypeRequest{
+		Name:        s.Name,
+		Description: s.Description,
+	}
+	id, err := uuid.Parse(s.ID)
+	if err != nil {
+		return out, fmt.Errorf("parse reference type id error: %s", err)
+	}
+	out.ID = id
+	return out, nil
 }
