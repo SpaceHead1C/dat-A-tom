37b1e74e1172e86a41701f9b32268292f9d21f05
SpaceHead1C <damh17@gmail.com>
2023-02-09 23:14:03 +0300

    test: reference type update method

--- test/ref_type_test.go
+++ test/ref_type_test.go
@@ -5,6 +5,8 @@ import (
 	. "datatom/internal/domain"
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestAddRefType(t *testing.T) {
@@ -18,4 +20,22 @@ func TestAddRefType(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(id.String())
+}
+
+func TestUpdateRefType(t *testing.T) {
+	mngr := newTestRefTypeManager(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	description := "dscr"
+	rt, err := mngr.Update(ctx, UpdRefTypeRequest{
+		ID:          uuid.MustParse("12345678-1234-1234-1234-123456789012"),
+		Description: &description,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("=== Reference type ===")
+	t.Log("ID:", rt.ID.String())
+	t.Log("name:", rt.Name)
+	t.Log("description:", rt.Description)
 }
