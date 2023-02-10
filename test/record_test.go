c7680bb0be24656d4eb0b3b68e12852e8e4d8dc6
SpaceHead1C <damh17@gmail.com>
2023-02-10 14:12:33 +0300

    test: record update method

--- test/record_test.go
+++ test/record_test.go
@@ -5,6 +5,8 @@ import (
 	"datatom/internal/domain"
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestAddRecord(t *testing.T) {
@@ -18,4 +20,26 @@ func TestAddRecord(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(id.String())
+}
+
+func TestUpdateRecord(t *testing.T) {
+	mngr := newTestRecordManager(t)
+	dm := true
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	o, err := mngr.Update(ctx, domain.UpdRecordRequest{
+		ID:           uuid.MustParse("12345678-1234-1234-1234-123456789012"),
+		DeletionMark: &dm,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("=== Record ===")
+	t.Log("ID:", o.ID.String())
+	t.Log("reference type ID:", o.ReferenceTypeID.String())
+	t.Log("name:", o.Name)
+	t.Log("description:", o.Description)
+	t.Log("deletion mark:", o.DeletionMark)
+	t.Log("hash sum:", o.Sum)
+	t.Log("change at:", o.ChangeAt)
 }
