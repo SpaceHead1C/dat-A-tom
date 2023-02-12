768d4328338fcc70b01d075c8d234fdc2372e4b2
SpaceHead1C <damh17@gmail.com>
2023-02-12 22:05:45 +0300

    test: property update method

--- test/property_test.go
+++ test/property_test.go
@@ -24,3 +24,26 @@ func TestAddProperty(t *testing.T) {
 	}
 	t.Log(id.String())
 }
+
+func TestUpdateProperty(t *testing.T) {
+	mngr := newTestPropertyManager(t)
+	description := "dscr"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	o, err := mngr.Update(ctx, domain.UpdPropertyRequest{
+		ID:          uuid.MustParse("12345678-1234-1234-1234-123456789012"),
+		Description: &description,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("=== Property ===")
+	t.Log("ID:", o.ID.String())
+	t.Log("name:", o.Name)
+	t.Log("description:", o.Description)
+	t.Log("types:", o.Types)
+	t.Log("reference types:", o.RefTypeIDs)
+	t.Log("owner type ID:", o.OwnerRefTypeID.String())
+	t.Log("hash sum:", o.Sum)
+	t.Log("change at:", o.ChangeAt)
+}
