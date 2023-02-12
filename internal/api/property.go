0a480b825fa76fb3a9c32b3d02a368853ab6a8ee
SpaceHead1C <damh17@gmail.com>
2023-02-12 18:26:34 +0300

    feat: new record add method

---
+++ internal/api/property.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"context"
+	. "datatom/internal/domain"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+const defaultPropertyManagerTimeout = time.Second
+
+type PropertyManager struct {
+	PropertyConfig
+}
+
+type PropertyConfig struct {
+	Repository PropertyRepository
+	Timeout    time.Duration
+}
+
+func NewPropertyManager(c PropertyConfig) (*PropertyManager, error) {
+	if c.Repository == nil {
+		return nil, fmt.Errorf("property repository can't be nil")
+	}
+	if c.Timeout == 0 {
+		c.Timeout = defaultPropertyManagerTimeout
+	}
+	return &PropertyManager{c}, nil
+}
+
+func (pm *PropertyManager) Add(ctx context.Context, req AddPropertyRequest) (uuid.UUID, error) {
+	ctx, cancel := context.WithTimeout(ctx, pm.Timeout)
+	defer cancel()
+	return pm.Repository.AddProperty(ctx, req)
+}
