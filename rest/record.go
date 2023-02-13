0b24099bff81b0751f2b96de7352f427f6b4a9af
SpaceHead1C <damh17@gmail.com>
2023-02-13 11:50:17 +0300

    refactor: from deprecated ioutil to io

--- rest/record.go
+++ rest/record.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -44,7 +43,7 @@ func newAddRecordHandler(s *server) http.HandlerFunc {
 
 func newUpdRecordHandler(s *server) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		b, err := ioutil.ReadAll(req.Body)
+		b, err := io.ReadAll(req.Body)
 		if err != nil {
 			s.textResp(w, http.StatusInternalServerError, "read body error")
 			s.logger.Errorf("read body error: %s", err)
@@ -75,7 +74,7 @@ func newUpdRecordHandler(s *server) http.HandlerFunc {
 
 func newPatchRecordHandler(s *server) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		b, err := ioutil.ReadAll(req.Body)
+		b, err := io.ReadAll(req.Body)
 		if err != nil {
 			s.textResp(w, http.StatusInternalServerError, "read body error")
 			s.logger.Errorf("read body error: %s", err)
