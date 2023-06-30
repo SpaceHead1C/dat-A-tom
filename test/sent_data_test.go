93a2788791c5d650e8f38b4a48528b2e4caef79d
SpaceHead1C <damh17@gmail.com>
2023-06-30 14:03:03 +0300

    test: set sent data

--- test/sent_data_test.go
+++ test/sent_data_test.go
@@ -8,6 +8,28 @@ import (
 	"time"
 )
 
+func TestSetSentData(t *testing.T) {
+	mngr := newTestSentDataManager(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	o, err := mngr.Set(ctx, domain.SetSentDataRequest{
+		RecordID:   uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+		PropertyID: uuid.MustParse("00000002-0000-0000-0000-000000000002"),
+		Sum:        "f53c863291026d25324f38a629a2806b0dad7388b021014398019ccabb3c2f39",
+		SentAt:     time.Date(2023, 4, 2, 10, 40, 14, 602917000, time.UTC),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(`=== Sent data ===
+record ID: %s
+property ID: %s
+hash sum: %s
+change at: %s`,
+		o.RecordID.String(), o.PropertyID.String(), o.Sum, o.SentAt,
+	)
+}
+
 func TestGetSentData(t *testing.T) {
 	mngr := newTestSentDataManager(t)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
