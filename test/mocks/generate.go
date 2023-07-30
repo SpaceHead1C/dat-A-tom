385231ab5a8106e8ef631b76c4b092cca78cb4a5
SpaceHead1C <damh17@gmail.com>
2023-07-31 00:40:51 +0300

    feat: go generate property mocks

--- test/mocks/generate.go
+++ test/mocks/generate.go
@@ -3,3 +3,7 @@ package mocks
 //go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RefTypeRepository --output "."
 
 //go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RefTypeBroker --output "."
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name PropertyRepository --output "."
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name PropertyBroker --output "."
