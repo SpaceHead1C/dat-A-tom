c2164416c940a42ab335367b35b7b0cf8c8ae0ac
SpaceHead1C <damh17@gmail.com>
2023-07-30 11:53:15 +0300

    feat: go generate reference type mocks

--- test/mocks/generate.go
+++ test/mocks/generate.go
@@ -1,1 +1,5 @@
 package mocks
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RefTypeRepository --output "."
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RefTypeBroker --output "."
