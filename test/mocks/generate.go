bf2096dd9ebc9f08f39714cf9d5346082bf98c33
SpaceHead1C <damh17@gmail.com>
2023-07-31 00:52:43 +0300

    feat: go generate record mocks

--- test/mocks/generate.go
+++ test/mocks/generate.go
@@ -7,3 +7,7 @@ package mocks
 //go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name PropertyRepository --output "."
 
 //go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name PropertyBroker --output "."
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RecordRepository --output "."
+
+//go:generate go run github.com/vektra/mockery/v2@latest --dir ../../internal/domain --name RecordBroker --output "."
